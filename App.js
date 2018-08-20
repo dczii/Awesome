31486dd91c924be749e92fd1c55277ee070888d8
Zii <dczii@live.com>
2018-08-20 10:48:52 +0800

    Apply load images

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { AppRegistry, Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import {Platform} from 'react-native';
+import _ from 'lodash';
+import { AppRegistry, CameraRoll, Button, StyleSheet, Text, TextInput, View, Image, ScrollView } from 'react-native';
+
+const majorVersionIOS = parseInt(Platform.Version, 10);
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,12 +14,27 @@ export default class App extends React.Component {
       age: '',
       country: '',
       view: true,
+      photos: {},
     };
   }
+
+  _handleButtonPress = () => {
+   CameraRoll.getPhotos({
+       first: 20,
+       assetType: 'Photos',
+     })
+     .then(r => {
+       this.setState({ photos: r.edges });
+     })
+     .catch((err) => {
+        //Error Loading Images
+     });
+   };
 
   render() {
     return (
       <View style={styles.container}>
+        <Text>{majorVersionIOS}</Text>
         <Image 
           style={{}}
           source={{uri: 'https://dczii.github.io/images/logo.png'}} style={{width: 100, height: 40}} />
@@ -63,6 +82,36 @@ export default class App extends React.Component {
             title='Edit'
             color="#841584" />
         }
+
+        {!_.isEmpty(this.state.photos) ? 
+          <Button
+            onPress={() => {this.setState({ photos: {} })}}
+            title='Clear Images'
+            color="#841584" />
+        :
+          <Button 
+            onPress={this._handleButtonPress}
+            title='Load Images'
+            color="#841584" />
+        }
+      
+
+         <ScrollView>
+         {_.map(this.state.photos, (p, i) => {
+            return (
+               <Image
+                 key={i}
+                 style={{
+                   width: 300,
+                   height: 100,
+                 }}
+                 source={{ uri: p.node.image.uri }}
+               />
+             );
+         })
+
+         }
+       </ScrollView>
       </View>
     );
   }
